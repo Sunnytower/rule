c4dd7271b950f582935048238667316c57f81eb0
jesse <77821870+Sunnytower@users.noreply.github.com>
2026-08-30 18:52:00 +0800

    add media_ip rules

---
+++ action/surge/stream_ip.py
@@ -0,0 +1,17 @@
+import os
+import requests
+urls = [
+"https://ruleset.skk.moe/List/ip/stream.conf",
+]
+result = []
+for url in urls:
+    resource_text = requests.get(url).text
+    for item in resource_text.split("\n"):
+        if (item not in result) and (not item.startswith('#')):
+            result.append(item)
+
+file_path = "./surge/media_ip.list"
+os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+with open(file_path, "w") as f:
+    f.write("\n".join(result))
